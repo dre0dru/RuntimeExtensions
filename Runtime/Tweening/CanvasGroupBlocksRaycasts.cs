9cb6490e83c8d28ff6073380e131eaaaf91c2748
Dre0Dru <dre0druofficial@gmail.com>
2022-12-18 19:05:32 +0600

    feat: added canvas group block raycast tween

---
+++ Runtime/Tweening/CanvasGroupBlocksRaycasts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Dre0Dru.Tweening
+{
+    [Serializable, AddTypeMenu("Canvas Group/Blocks Raycasts")]
+    public class CanvasGroupBlocksRaycasts : TweenBase
+    {
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+
+        [SerializeField]
+        private bool _initialBlocksRaycasts = false;
+
+        [SerializeField]
+        private bool _targetBlocksRaycasts = true;
+
+        public override void ResetToInitialValues()
+        {
+            _canvasGroup.blocksRaycasts = _initialBlocksRaycasts;
+        }
+
+        protected override Tween Create() =>
+            DOVirtual.DelayedCall(_duration, () => _canvasGroup.blocksRaycasts = _targetBlocksRaycasts);
+    }
+}
