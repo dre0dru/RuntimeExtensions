d4abe2e6a2aead89326585e31f388c177da5c8d5
Dre0Dru <dre0druofficial@gmail.com>
2023-01-12 23:12:15 +0300

    feat: added transform extensions

---
+++ Runtime/ObjectExtensions/TransformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Dre0Dru.ObjectExtensions
+{
+    public static class TransformExtensions
+    {
+        public static void DestroyChildren(this Transform transform)
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                var child = transform.GetChild(i);
+                child.Unparent();
+                Object.Destroy(child.gameObject);
+            }
+        }
+
+        public static void Unparent(this Transform transform) =>
+            transform.SetParent(null);
+    }
+}
