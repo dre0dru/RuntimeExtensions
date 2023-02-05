d80399ef9a6a18f7e37c8863018e3169c1190707
Dre0Dru <dre0druofficial@gmail.com>
2023-02-05 14:17:21 +0300

    feat: added cached components

---
+++ Runtime/CachedComponents/CachedComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Dre0Dru.CachedComponents
+{
+    public class CachedComponent<TComponent>
+        where TComponent : class
+    {
+        private readonly GameObject _source;
+        private TComponent _component;
+
+        public TComponent Component => _component ??= _source.GetComponent<TComponent>();
+
+        public CachedComponent(GameObject source)
+        {
+            _source = source;
+        }
+    }
+}
---
+++ Runtime/CachedComponents/CachedTransform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Dre0Dru.CachedComponents
+{
+    public class CachedTransform : CachedComponent<Transform>
+    {
+        public CachedTransform(GameObject source) : base(source)
+        {
+        }
+    }
+}
