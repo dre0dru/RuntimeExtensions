e9aae03c139ad0bed83a09ad341c64c6fc48d620
Dre0Dru <dre0druofficial@gmail.com>
2023-08-20 18:07:09 +0300

    feat: genetic base state and state machine

--- Runtime/FSM/BaseState.cs
+++ Runtime/FSM/BaseState.cs
@@ -43,4 +43,89 @@ namespace Dre0Dru.FSM
         {
         }
     }
+    
+    public class BaseState<TBaseState> : IState
+        where TBaseState : BaseStateComponent<TBaseState>
+    {
+        bool IState.CanEnterState<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            return CanEnterState(from as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        bool IState.CanExitState<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            return CanExitState(to as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        void IState.OnStateEntered<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            OnStateEntered(from as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        void IState.OnStateExited<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            OnStateExited(to as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        protected virtual bool CanEnterState(TBaseState from, IStateMachine<TBaseState> stateMachine)
+        {
+            return true;
+        }
+        
+        protected virtual bool CanExitState(TBaseState to, IStateMachine<TBaseState> stateMachine)
+        {
+            return true;
+        }
+
+        protected virtual void OnStateEntered(TBaseState from, IStateMachine<TBaseState> stateMachine)
+        {
+        }
+        
+        protected virtual void OnStateExited(TBaseState to, IStateMachine<TBaseState> stateMachine)
+        {
+        }
+    }
+    
+    public class BaseState<TBaseState, TStateMachine> : IState
+        where TBaseState : BaseStateComponent<TBaseState, TStateMachine>
+        where TStateMachine : class, IStateMachine<TBaseState>
+    {
+        bool IState.CanEnterState<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            return CanEnterState(from as TBaseState, stateMachine as TStateMachine);
+        }
+
+        bool IState.CanExitState<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            return CanExitState(to as TBaseState, stateMachine as TStateMachine);
+        }
+
+        void IState.OnStateEntered<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            OnStateEntered(from as TBaseState, stateMachine as TStateMachine);
+        }
+
+        void IState.OnStateExited<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            OnStateExited(to as TBaseState, stateMachine as TStateMachine);
+        }
+
+        protected virtual bool CanEnterState(TBaseState from, TStateMachine stateMachine)
+        {
+            return true;
+        }
+        
+        protected virtual bool CanExitState(TBaseState to, TStateMachine stateMachine)
+        {
+            return true;
+        }
+
+        protected virtual void OnStateEntered(TBaseState from, TStateMachine stateMachine)
+        {
+        }
+        
+        protected virtual void OnStateExited(TBaseState to, TStateMachine stateMachine)
+        {
+        }
+    }
 }
--- Runtime/FSM/BaseStateComponent.cs
+++ Runtime/FSM/BaseStateComponent.cs
@@ -6,40 +6,157 @@ namespace Dre0Dru.FSM
     {
         bool IState.CanEnterState<TState>(TState from, IStateMachine<TState> stateMachine)
         {
-            return CanEnterState(from as BaseState, stateMachine as IStateMachine<BaseState>);
+            return CanEnterState(from as BaseStateComponent, stateMachine as IStateMachine<BaseStateComponent>);
         }
 
         bool IState.CanExitState<TState>(TState to, IStateMachine<TState> stateMachine)
         {
-            return CanExitState(to as BaseState, stateMachine as IStateMachine<BaseState>);
+            return CanExitState(to as BaseStateComponent, stateMachine as IStateMachine<BaseStateComponent>);
         }
 
         void IState.OnStateEntered<TState>(TState from, IStateMachine<TState> stateMachine)
         {
-            OnStateEntered(from as BaseState, stateMachine as IStateMachine<BaseState>);
+            enabled = true;
+            
+            OnStateEntered(from as BaseStateComponent, stateMachine as IStateMachine<BaseStateComponent>);
         }
 
         void IState.OnStateExited<TState>(TState to, IStateMachine<TState> stateMachine)
         {
-            OnStateExited(to as BaseState, stateMachine as IStateMachine<BaseState>);
+            enabled = false;
+            
+            OnStateExited(to as BaseStateComponent, stateMachine as IStateMachine<BaseStateComponent>);
         }
 
-        protected virtual bool CanEnterState(BaseState from, IStateMachine<BaseState> stateMachine)
+        protected virtual bool CanEnterState(BaseStateComponent from, IStateMachine<BaseStateComponent> stateMachine)
         {
             return true;
         }
         
-        protected virtual bool CanExitState(BaseState to, IStateMachine<BaseState> stateMachine)
+        protected virtual bool CanExitState(BaseStateComponent to, IStateMachine<BaseStateComponent> stateMachine)
         {
             return true;
         }
 
-        protected virtual void OnStateEntered(BaseState from, IStateMachine<BaseState> stateMachine)
+        protected virtual void OnStateEntered(BaseStateComponent from, IStateMachine<BaseStateComponent> stateMachine)
         {
         }
         
-        protected virtual void OnStateExited(BaseState to, IStateMachine<BaseState> stateMachine)
+        protected virtual void OnStateExited(BaseStateComponent to, IStateMachine<BaseStateComponent> stateMachine)
         {
         }
+    }
+    
+    public class BaseStateComponent<TBaseState> : MonoBehaviour, IState
+        where TBaseState : BaseStateComponent<TBaseState>
+    {
+        bool IState.CanEnterState<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            return CanEnterState(from as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        bool IState.CanExitState<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            return CanExitState(to as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        void IState.OnStateEntered<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            enabled = true;
+            
+            OnStateEntered(from as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        void IState.OnStateExited<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            enabled = false;
+            
+            OnStateExited(to as TBaseState, stateMachine as IStateMachine<TBaseState>);
+        }
+
+        protected virtual bool CanEnterState(TBaseState from, IStateMachine<TBaseState> stateMachine)
+        {
+            return true;
+        }
+        
+        protected virtual bool CanExitState(TBaseState to, IStateMachine<TBaseState> stateMachine)
+        {
+            return true;
+        }
+
+        protected virtual void OnStateEntered(TBaseState from, IStateMachine<TBaseState> stateMachine)
+        {
+        }
+        
+        protected virtual void OnStateExited(TBaseState to, IStateMachine<TBaseState> stateMachine)
+        {
+        }
+        
+        #if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+                return;
+
+            enabled = false;
+        }
+        #endif
+    }
+    
+    public class BaseStateComponent<TBaseState, TStateMachine> : MonoBehaviour, IState
+        where TBaseState : BaseStateComponent<TBaseState, TStateMachine>
+        where TStateMachine : class, IStateMachine<TBaseState>
+    {
+        bool IState.CanEnterState<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            return CanEnterState(from as TBaseState, stateMachine as TStateMachine);
+        }
+
+        bool IState.CanExitState<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            return CanExitState(to as TBaseState, stateMachine as TStateMachine);
+        }
+
+        void IState.OnStateEntered<TState>(TState from, IStateMachine<TState> stateMachine)
+        {
+            enabled = true;
+            
+            OnStateEntered(from as TBaseState, stateMachine as TStateMachine);
+        }
+
+        void IState.OnStateExited<TState>(TState to, IStateMachine<TState> stateMachine)
+        {
+            enabled = false;
+            
+            OnStateExited(to as TBaseState, stateMachine as TStateMachine);
+        }
+
+        protected virtual bool CanEnterState(TBaseState from, TStateMachine stateMachine)
+        {
+            return true;
+        }
+        
+        protected virtual bool CanExitState(TBaseState to, TStateMachine stateMachine)
+        {
+            return true;
+        }
+
+        protected virtual void OnStateEntered(TBaseState from, TStateMachine stateMachine)
+        {
+        }
+        
+        protected virtual void OnStateExited(TBaseState to, TStateMachine stateMachine)
+        {
+        }
+        
+        #if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+                return;
+
+            enabled = false;
+        }
+        #endif
     }
 }
