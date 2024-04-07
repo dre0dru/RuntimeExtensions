c10c82709c92eded3c41ca9908ac40e19eda861d
Dre0Dru <dre0druofficial@gmail.com>
2024-04-07 14:06:49 +0300

    feat: added layers extensions

--- Runtime/RigidbodyPhysics/LayersExtensions.cs
+++ Runtime/RigidbodyPhysics/LayersExtensions.cs
@@ -9,6 +9,16 @@ namespace Dre0Dru.RigidbodyPhysics
             return layerMask == (layerMask | (1 << layer));
         }
 
+        public static bool IsInLayerMask(this LayerMask layerMask, GameObject gameObject)
+        {
+            return layerMask.IsInLayerMask(gameObject.layer);
+        }
+
+        public static bool IsInLayerMask(this LayerMask layerMask, Component component)
+        {
+            return layerMask.IsInLayerMask(component.gameObject);
+        }
+
         public static bool HasNothing(this LayerMask layerMask)
         {
             return layerMask == 0;
