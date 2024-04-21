5d12ef937c057f728cd9a4a23c88d1c93fe49693
Dre0Dru <dre0druofficial@gmail.com>
2024-04-21 12:28:00 +0300

    feat: quaternion look extensions

--- Runtime/MathExtensions/QuaternionExtensions.cs
+++ Runtime/MathExtensions/QuaternionExtensions.cs
@@ -8,5 +8,23 @@ namespace Dre0Dru.MathExtensions
         {
             return quaternion * Vector3.forward;
         }
+
+        public static bool TryGetLookRotation(this Vector3 forward, out Quaternion rotation)
+        {
+            return forward.TryGetLookRotation(Vector3.up, out rotation);
+        }
+
+        public static bool TryGetLookRotation(this Vector3 forward, Vector3 up, out Quaternion rotation)
+        {
+            rotation = Quaternion.identity;
+
+            if (forward == Vector3.zero)
+            {
+                return false;
+            }
+
+            rotation = Quaternion.LookRotation(forward, up);
+            return true;
+        }
     }
 }
