a99cb792b1a1ae3d40c2f4231a6c7d8aadc531b1
Dre0Dru <dre0druofficial@gmail.com>
2024-06-12 16:38:16 +0300

    feat: added new vector and math extensions

---
+++ Runtime/MathExtensions/MathExtensions.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+namespace Dre0Dru.MathExtensions
+{
+    public static class MathExtensions
+    {
+        public const float FloatZeroTolerance = 0.0000000001f;
+
+        public static float Remap(float from1, float from2, float to1, float to2, float value)
+        {
+            var t = Mathf.InverseLerp(from1, from2, value);
+            return Mathf.Lerp(to1, to2, t);
+        }
+
+        /// <summary>
+        /// Checks whether value is near to zero within a tolerance.
+        /// </summary>
+        public static bool IsCloseToZero(this float value)
+        {
+            return Mathf.Abs(value) < FloatZeroTolerance;
+        }
+
+        /// <summary>
+        /// Clamps given angle within min - max range.
+        /// </summary>
+        public static float ClampAngle(float a, float min, float max)
+        {
+            while (max < min)
+                max += 360.0f;
+
+            while (a > max)
+                a -= 360.0f;
+
+            while (a < min)
+                a += 360.0f;
+
+            return a > max ? a - (max + min) * 0.5f < 180.0f ? max : min : a;
+        }
+
+        /// <summary>
+        /// Returns Angle in the range (0, 360)
+        /// </summary>
+        public static float ClampAngle(float angle)
+        {
+            // returns angle in the range (-360, 360)
+
+            angle = angle % 360.0f;
+
+            if (angle < 0.0f)
+            {
+                // shift to (0, 360) range
+
+                angle += 360.0f;
+            }
+
+            return angle;
+        }
+
+        /// <summary>
+        /// Return angle in range -180 to 180
+        /// </summary>
+        public static float NormalizeAngle(float angle)
+        {
+            // returns angle in the range (0, 360)
+
+            angle = ClampAngle(angle);
+
+            if (angle > 180.0f)
+            {
+                // shift to (-180,180)
+
+                angle -= 360.0f;
+            }
+
+            return angle;
+        }
+
+        /// <summary>
+        /// Clamps the given angle into 0 - 360 degrees range.
+        /// </summary>
+        private static float Clamp0360(float eulerAngles)
+        {
+            float result = eulerAngles - Mathf.CeilToInt(eulerAngles / 360f) * 360f;
+            if (result < 0) result += 360f;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a new rotation angle (interpolated) clamped in the range (0.0f , 360.0f)
+        /// </summary>
+        public static float FixedTurn(float current, float target, float maxDegreesDelta)
+        {
+            if (maxDegreesDelta == 0.0f)
+                return Clamp0360(current);
+
+            if (maxDegreesDelta >= 360.0f)
+                return Clamp0360(target);
+
+            float result = Clamp0360(current);
+            current = result;
+            target = Clamp0360(target);
+
+            if (current > target)
+            {
+                if (current - target < 180.0f)
+                    result -= Mathf.Min(current - target, Mathf.Abs(maxDegreesDelta));
+                else
+                    result += Mathf.Min(target + 360.0f - current, Mathf.Abs(maxDegreesDelta));
+            }
+            else
+            {
+                if (target - current < 180.0f)
+                    result += Mathf.Min(target - current, Mathf.Abs(maxDegreesDelta));
+                else
+                    result -= Mathf.Min(current + 360.0f - target, Mathf.Abs(maxDegreesDelta));
+            }
+
+            return Clamp0360(result);
+        }
+
+        /// <summary>
+        /// Frame Rate Independent Damping.
+        /// Source: https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+        /// </summary>
+        public static float Damp(float a, float b, float lambda, float dt)
+        {
+            return Mathf.Lerp(a, b, 1.0f - Mathf.Exp(-lambda * dt));
+        }
+
+        /// <summary>
+        /// Frame Rate Independent Damping.
+        /// Source: https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+        /// </summary>
+        public static Vector3 Damp(Vector3 a, Vector3 b, float lambda, float dt)
+        {
+            return Vector3.Lerp(a, b, 1.0f - Mathf.Exp(-lambda * dt));
+        }
+    }
+}
--- Runtime/MathExtensions/VectorExtensions.cs
+++ Runtime/MathExtensions/VectorExtensions.cs
@@ -4,6 +4,15 @@ namespace Dre0Dru.MathExtensions
 {
     public static class VectorExtensions
     {
+        public const float MagnitudeZeroTolerance = 9.99999943962493E-11f;
+
+        public static Vector3 FlattenX(this Vector3 vector3)
+        {
+            vector3.x = 0;
+
+            return vector3;
+        }
+
         public static Vector3 FlattenY(this Vector3 vector3)
         {
             vector3.y = 0;
@@ -11,9 +20,11 @@ namespace Dre0Dru.MathExtensions
             return vector3;
         }
 
-        public static Vector2 ToLocalSpaceXZ(this Vector2 vector2, Transform transform)
+        public static Vector3 FlattenZ(this Vector3 vector3)
         {
-            return vector2.ToVector3XZ().ToLocalSpace(transform).ToVector2XZ();
+            vector3.z = 0;
+
+            return vector3;
         }
 
         public static Vector2 ToVector2XZ(this Vector3 vector3)
@@ -29,11 +40,6 @@ namespace Dre0Dru.MathExtensions
         public static Vector2 ToVector2XY(this Vector3 vector3)
         {
             return new Vector2(vector3.x, vector3.y);
-        }
-
-        public static Vector3 ToLocalSpace(this Vector3 vector3, Transform transform)
-        {
-            return transform.InverseTransformDirection(vector3);
         }
 
         public static Vector3 ToVector3XZ(this Vector2 vector2, float y = 0)
@@ -59,14 +65,148 @@ namespace Dre0Dru.MathExtensions
             return new Vector2(xDir, zDir).normalized;
         }
 
-        public static Quaternion ToQuaternion(this Vector3 vector3)
+        public static (Vector3 vertical, Vector3 lateral) SplitIntoVerticalAndLateral(this Vector3 vector3, Vector3 up)
+        {
+            var vertical = Vector3.Project(vector3, up);
+            var lateral = vector3 - vertical;
+
+            return (vertical, lateral);
+        }
+
+        public static Quaternion ToLookRotation(this Vector3 vector3)
         {
             return Quaternion.LookRotation(vector3);
         }
 
-        public static Quaternion ToQuaternion(this Vector3 vector3, Vector3 up)
+        public static Quaternion ToLookRotation(this Vector3 vector3, Vector3 up)
         {
             return Quaternion.LookRotation(vector3, up);
+        }
+
+        /// <summary>
+        /// Checks whether value is near to zero within a tolerance.
+        /// </summary>
+        public static bool IsCloseToZero(this Vector2 vector2)
+        {
+            return vector2.sqrMagnitude < MagnitudeZeroTolerance;
+        }
+
+        /// <summary>
+        /// Checks whether value is near to zero within a tolerance.
+        /// </summary>
+        public static bool IsCloseToZero(this Vector3 vector3)
+        {
+            return vector3.sqrMagnitude < MagnitudeZeroTolerance;
+        }
+
+        /// <summary>
+        /// Checks whether vector is exceeding the magnitude within a small error tolerance.
+        /// </summary>
+        public static bool IsExceeding(this Vector3 vector3, float magnitude)
+        {
+            // Allow 1% error tolerance, to account for numeric imprecision.
+            const float kErrorTolerance = 1.01f;
+
+            return vector3.sqrMagnitude > magnitude * magnitude * kErrorTolerance;
+        }
+
+        /// <summary>
+        /// Returns a copy of given vector with a magnitude of 1,
+        /// and outs its magnitude before normalization.
+        ///
+        /// If the vector is too small to be normalized a zero vector will be returned.
+        /// </summary>
+        public static Vector3 Normalized(this Vector3 vector3, out float magnitude)
+        {
+            magnitude = vector3.magnitude;
+            //The same as Vector3 implementation
+            if (magnitude > 9.99999974737875E-06)
+                return vector3 / magnitude;
+
+            magnitude = 0.0f;
+
+            return Vector3.zero;
+        }
+
+        public static float Dot(this Vector3 vector3, Vector3 otherVector3)
+        {
+            return Vector3.Dot(vector3, otherVector3);
+        }
+
+        public static Vector3 ProjectOn(this Vector3 thisVector, Vector3 normal)
+        {
+            return Vector3.Project(thisVector, normal);
+        }
+
+        public static Vector3 ProjectOnPlane(this Vector3 thisVector, Vector3 planeNormal)
+        {
+            return Vector3.ProjectOnPlane(thisVector, planeNormal);
+        }
+
+        public static Vector3 ProjectPointOnPlane(Vector3 point, Vector3 planeOrigin, Vector3 planeNormal)
+        {
+            var toPoint = point - planeOrigin;
+            var toPointProjected = Vector3.Project(toPoint, planeNormal);
+
+            return point - toPointProjected;
+        }
+
+        public static Vector3 ClampMagnitude(this Vector3 vector3, float maxLength)
+        {
+            return Vector3.ClampMagnitude(vector3, maxLength);
+        }
+
+        /// <summary>
+        /// Returns a copy of given vector perpendicular to other vector.
+        /// </summary>
+        public static Vector3 PerpendicularTo(this Vector3 vector3, Vector3 otherVector)
+        {
+            return Vector3.Cross(vector3, otherVector).normalized;
+        }
+
+        /// <summary>
+        /// Returns a copy of given vector adjusted to be tangent to a specified surface normal relatively to given up axis.
+        /// </summary>
+        public static Vector3 TangentTo(this Vector3 vector3, Vector3 normal, Vector3 up)
+        {
+            return vector3.GetTangent(normal, up) * vector3.magnitude;
+        }
+
+        public static Vector3 GetTangent(this Vector3 direction, Vector3 normal, Vector3 up)
+        {
+            var right = direction.PerpendicularTo(up);
+
+            return normal.PerpendicularTo(right);
+        }
+
+        /// <summary>
+        /// Transforms a vector to be relative to given transform.
+        /// If isPlanar == true, the transform will be applied on the plane defined by world up axis.
+        /// </summary>
+        public static Vector3 RelativeTo(this Vector3 vector3, Transform relativeTransform, bool isPlanar = true)
+        {
+            return vector3.RelativeTo(relativeTransform, Vector3.up, isPlanar);
+        }
+
+        /// <summary>
+        /// Transforms a vector to be relative to given transform.
+        /// If isPlanar == true, the transform will be applied on the plane defined by upAxis.
+        /// </summary>
+        public static Vector3 RelativeTo(this Vector3 vector3, Transform relativeTransform, Vector3 upAxis, bool isPlanar = true)
+        {
+            var forward = relativeTransform.forward;
+
+            if (isPlanar)
+            {
+                forward = forward.ProjectOnPlane(upAxis);
+
+                if (forward.IsCloseToZero())
+                {
+                    forward = relativeTransform.up.ProjectOnPlane(upAxis);
+                }
+            }
+
+            return forward.ToLookRotation() * vector3;
         }
     }
 }
